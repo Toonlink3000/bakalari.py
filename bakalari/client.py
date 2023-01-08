81464ca921a2c401dc22633e9e79e2cddc11a6ef
Toonlink3000 <dannyprague@seznam.cz>
2023-01-08 13:18:29 +0100

    started work on user module and authentication checking

--- bakalari/client.py
+++ bakalari/client.py
@@ -1,4 +1,20 @@
 import requests
+import user
+from exceptions import NotAuthenticatedError
+
+class authenticated_method():
+	def __init__(self, func):
+		self.func = func
+
+	def __call__(self, parent_class, *args, **kwargs):
+		if parent_class.authenticated == True:
+			result = self.func(parent_class, *args, **kwargs)
+
+		else:
+			raise NotAuthenticatedError(type(parent_class).__name__, parent_class.__name__, self.func.__name__)
+			raise
+
+		return result
 
 def get_city_json():
 	request = requests.get("https://sluzby.bakalari.cz/api/v1/municipality", headers={"Accept":"application/json"})
@@ -10,16 +26,26 @@ def get_school_json(city:str):
 
 	return request.json()
 
+def get_campaign_banners(location:str, category_code:str):
+	url = "https://campaign.bakalari.cz/bannerinfo/${}/${}".format(location, category_code)
+	request = requests.get(url)
+	result = requests.json()
+
+	if "banners" not in result.keys():
+		return result
+
+	return result["banners"]
+
 class Client():
 	authenticated = False
 
 	@classmethod
 	def login(cls, adress:str, username:str, password:str, login_path = "/api/login"):
 		payload = {
-						"client_id": "ANDR",
-						"grant_type": "password",
-						"username":username, 
-						"password": password
+			"client_id": "ANDR",
+			"grant_type": "password",
+			"username": username, 
+			"password": password
 		}
 
 		request = requests.post( adress + login_path, data=payload)
@@ -30,6 +56,7 @@ class Client():
 			return
 
 		client = cls()
+		client.api_adress = adress
 
 		if "bak:UserId" in value.keys():
 			client.user_id = value["bak:UserId"]
@@ -42,6 +69,14 @@ class Client():
 			client.authenticated = True
 
 		return client
+
+	@authenticated_method
+	def get_user(self) -> user.User:
+		head = {
+			"Content-Type": "application/x-www-form-urlencoded",
+			"Authorization": f"Bearer {self.access_token}"
+		}
+		request = requests.get(, headers)
 
 	def logout(self):
 		pass
---
+++ bakalari/user.py
@@ -0,0 +1,8 @@
+import json
+
+class User():
+	def __init__(json_data):
+		
+
+	def get_campaign_category_code(self) -> str:
+		pass
